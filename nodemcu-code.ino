8ae44f45430420ae49b9e922485c572f675de6a2
Kartik-Developer-19 <modipreeti1111@gmail.com>
2025-05-01 22:20:06 +0530

    Updated nodemcu-code.ino

--- nodemcu-code.ino
+++ nodemcu-code.ino
@@ -13,42 +13,65 @@ const int touchSensorPin = D1; // Change D1 to the appropriate pin number for yo
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Automation</title>
     <style>
-        *{
+        * {
             margin: 0;
             padding: 0;
             box-sizing: border-box;
             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
         }
-        button{
+
+        button {
             font-size: 1.3em;
             padding: 7px 15px;
             border-radius: 10px;
             border: none;
             font-weight: 600;
             color: white;
+            background-color: burlywood;
         }
-        body, html{
+
+        body,
+        html {
             width: 100%;
             height: 100%;
         }
-        h1{
+
+        h1 {
             font-size: 3.5em;
             text-align: center;
             margin-bottom: 15px;
         }
-        h2{
+
+        h2 {
             text-align: center;
             font-size: 2.3em;
         }
-        h3{
+
+        h3 {
             font-size: 1.4em;
         }
-        .hall, .bedroom{
+
+        .btn-cont {
+            display: flex;
+            justify-content: center;
+            gap: 10px;
+            align-items: center;
+        }
+
+        .start h1 {
+            font-size: 2em;
+            margin-top: 20px;
+        }
+
+        .hall,
+        .bedroom,
+        .kitchen {
             padding: 5px 10px;
             margin: 10px;
             display: flex;
@@ -56,8 +79,13 @@ const char index_html[] PROGMEM = R"rawliteral(
             justify-content: center;
             gap: 20px;
             flex-direction: column;
+            display: none;
         }
-        .lights, .fan, .tv{
+
+        .lights,
+        .normalLights,
+        .fan,
+        .tv {
             padding: 5px 10px;
             margin: 10px;
             display: flex;
@@ -65,34 +93,63 @@ const char index_html[] PROGMEM = R"rawliteral(
             justify-content: center;
             gap: 20px;
         }
-        .on{
+
+        .on {
             background-color: rgb(30, 211, 30);
         }
-        .off{
+
+        .off {
             background-color: rgb(255, 91, 91);
         }
 
         @media (max-width: 600px) {
-            .hall, .bedroom, .lights, .fan, .tv{
+
+            .hall,
+            .bedroom,
+            .lights,
+            .normalLights,
+            .fan,
+            .tv {
                 display: flex;
                 align-items: center;
                 justify-content: center;
                 flex-direction: column;
             }
+
+            .hall,
+            .bedroom,
+            .kitchen {
+                display: none;
+            }
         }
-        
     </style>
 </head>
+
 <body>
     <h1>Control Your Home</h1>
-
-    <h2>Hall Controls</h2>
+    <div class="btn-cont">
+        <button class="hall-btn">Hall</button>
+        <button class="bed-btn">Bedroom</button>
+        <button class="kitchen-btn">Kitchen</button>
+    </div>
+    <div class="start">
+        <h1>Choose Button for each rooms</h1>
+    </div>
     <div class="hall">
+        <div class="btn-cont">
+            <button class="ambience">Ambience Light Mode</button>
+            <button class="normal">Normal Light Mode</button>
+        </div>
         <div class="lights">
             <h3>Lights</h3>
             <button class="on" onclick="hallfunon()">Fun Mode</button>
             <button class="on" onclick="hallseriouson()">Serious Mode</button>
             <button class="on" onclick="hallrelaxon()">Relaxed Mode</button>
+            <button class="off" onclick="hallbulboff()">Hall Light Off</button>
+        </div>
+        <div class="normalLights">
+            <h3>Lights</h3>
+            <button class="on" onclick="hallbulboff()">Hall Light On</button>
             <button class="off" onclick="hallbulboff()">Hall Light Off</button>
         </div>
         <div class="fan">
@@ -104,19 +161,27 @@ const char index_html[] PROGMEM = R"rawliteral(
         </div>
         <div class="tv">
             <h3>TV</h3>
-            <button class="on" onclick="tvOn()">TV ON</button>
-            <button class="off" onclick="tvOff()">TV OFF</button>
+            <button class="on" onclick="tvOff()">TV ON</button>
+            <button class="off" onclick="tvOn()">TV OFF</button>
         </div>
-        
     </div>
-    <h2>Bedroom Controls</h2>
+    <!-- <h2>Bedroom Controls</h2> -->
     <div class="bedroom">
+        <div class="btn-cont">
+            <button class="ambience">Ambience Light Mode</button>
+            <button class="normal">Normal Light Mode</button>
+        </div>
         <div class="lights">
             <h3>Lights</h3>
             <button class="on" onclick="bedfunon()">Fun Mode</button>
             <button class="on" onclick="bedseriouson()">Serious Mode</button>
             <button class="on" onclick="bedrelaxon()">Relaxed Mode</button>
             <button class="off" onclick="bedbulboff()">Bedroom Light Off</button>
+        </div>
+        <div class="normalLights">
+            <h3>Lights</h3>
+            <button class="on" onclick="hallbulboff()">Bedroom Light On</button>
+            <button class="off" onclick="hallbulboff()">Bedroom Light Off</button>
         </div>
         <div class="fan">
             <h3>Fan</h3>
@@ -126,109 +191,194 @@ const char index_html[] PROGMEM = R"rawliteral(
             <button class="off" onclick="bedFanOff()">Fan OFF</button>
         </div>
     </div>
-    
+    <div class="kitchen">
+        <div class="lights">
+            <h3>Lights</h3>
+            <button class="on" onclick="hallfunon()">Fun Mode</button>
+            <button class="on" onclick="hallseriouson()">Serious Mode</button>
+            <button class="on" onclick="hallrelaxon()">Relaxed Mode</button>
+            <button class="off" onclick="hallbulboff()">Hall Light Off</button>
+        </div>
+    </div>
+
     <script>
+        let hall_btn = document.querySelector(".hall-btn")
+        let bed_btn = document.querySelector(".bed-btn")
+        let kitchen_btn = document.querySelector(".kitchen-btn")
+        let hall = document.querySelector(".hall")
+        let start = document.querySelector(".start")
+        let bedroom = document.querySelector(".bedroom")
+        let kitchen = document.querySelector(".kitchen")
+        let hallAmbienceLight_btn = document.querySelector(".hall .btn-cont .ambience")
+        let hallAmbienceLight = document.querySelector(".hall .lights")
+        let hallNormalLight_btn = document.querySelector(".hall .btn-cont .normal")
+        let hallNormalLight = document.querySelector(".hall .normalLights")
+        let bedAmbienceLight_btn = document.querySelector(".bedroom .btn-cont .ambience")
+        let bedAmbienceLight = document.querySelector(".bedroom .lights")
+        let bedNormalLight_btn = document.querySelector(".bedroom .btn-cont .normal")
+        let bedNormalLight = document.querySelector(".bedroom .normalLights")
+
+        hall_btn.addEventListener("click", () => {
+            hall_btn.style.backgroundColor = "rgb(248, 165, 57)"
+            bed_btn.style.backgroundColor = "burlywood"
+            kitchen_btn.style.backgroundColor = "burlywood"
+
+            hall.style.display = "inline"
+            bedroom.style.display = "none"
+            kitchen.style.display = "none"
+            start.style.display = "none"
+        })
+        bed_btn.addEventListener("click", () => {
+            hall_btn.style.backgroundColor = "burlywood"
+            bed_btn.style.backgroundColor = "rgb(248, 165, 57)"
+            kitchen_btn.style.backgroundColor = "burlywood"
+
+            hall.style.display = "none"
+            bedroom.style.display = "inline"
+            kitchen.style.display = "none"
+            start.style.display = "none"
+        })
+        kitchen_btn.addEventListener("click", () => {
+            hall_btn.style.backgroundColor = "burlywood"
+            bed_btn.style.backgroundColor = "burlywood"
+            kitchen_btn.style.backgroundColor = "rgb(248, 165, 57)"
+
+            hall.style.display = "none"
+            bedroom.style.display = "none"
+            kitchen.style.display = "inline"
+            start.style.display = "none"
+        })
+        hallAmbienceLight_btn.addEventListener("click", () => {
+            hallAmbienceLight_btn.style.backgroundColor = "rgb(248, 165, 57)"
+            hallNormalLight_btn.style.backgroundColor = "burlywood"
+
+            hallAmbienceLight.style.display = "flex"
+            hallNormalLight.style.display = "none"
+        })
+        hallNormalLight_btn.addEventListener("click", () => {
+            hallAmbienceLight_btn.style.backgroundColor = "burlywood"
+            hallNormalLight_btn.style.backgroundColor = "rgb(248, 165, 57)"
+
+            hallAmbienceLight.style.display = "none"
+            hallNormalLight.style.display = "flex"
+        })
+        bedAmbienceLight_btn.addEventListener("click", () => {
+            bedAmbienceLight_btn.style.backgroundColor = "rgb(248, 165, 57)"
+            bedNormalLight_btn.style.backgroundColor = "burlywood"
+
+            bedAmbienceLight.style.display = "flex"
+            bedNormalLight.style.display = "none"
+        })
+        bedNormalLight_btn.addEventListener("click", () => {
+            bedAmbienceLight_btn.style.backgroundColor = "burlywood"
+            bedNormalLight_btn.style.backgroundColor = "rgb(248, 165, 57)"
+
+            bedAmbienceLight.style.display = "none"
+            bedNormalLight.style.display = "flex"
+        })
+
         // Hall 
-            // Lights 
-        function hallfunon(){
+        // Lights 
+        function hallfunon() {
             fetch('/hallfunon')
                 .then(response => response.text())
                 .then(data => console.log(data))
         }
-        function hallseriouson(){
+        function hallseriouson() {
             fetch('/hallseriouson')
                 .then(response => response.text())
                 .then(data => console.log(data))
         }
-        function hallrelaxon(){
+        function hallrelaxon() {
             fetch('/hallrelaxon')
                 .then(response => response.text())
                 .then(data => console.log(data))
         }
-        function hallbulboff(){
+        function hallbulboff() {
             fetch('/hallbulboff')
                 .then(response => response.text())
                 .then(data => console.log(data))
         }
-            // Fans 
-        function hallFanSlow(){
+        // Fans 
+        function hallFanSlow() {
             fetch('/hallFanSlow')
                 .then(response => response.text())
                 .then(data => console.log(data))
         }
-        function hallFanMedium(){
+        function hallFanMedium() {
             fetch('/hallFanMedium')
                 .then(response => response.text())
                 .then(data => console.log(data))
         }
-        function hallFanFast(){
+        function hallFanFast() {
             fetch('/hallFanFast')
                 .then(response => response.text())
                 .then(data => console.log(data))
         }
-        function hallFanOff(){
+        function hallFanOff() {
             fetch('/hallFanOff')
                 .then(response => response.text())
                 .then(data => console.log(data))
         }
-            // TV
-        function tvOn(){
+        // TV
+        function tvOn() {
             fetch('/tvOn')
                 .then(response => response.text())
                 .then(data => console.log(data))
         }
-        function tvOff(){
+        function tvOff() {
             fetch('/tvOff')
                 .then(response => response.text())
                 .then(data => console.log(data))
         }
-        
+
 
         //Bedroom
-            // Lights 
-        function bedfunon(){
+        // Lights 
+        function bedfunon() {
             fetch('/bedfunon')
                 .then(response => response.text())
                 .then(data => console.log(data))
         }
-        function bedseriouson(){
+        function bedseriouson() {
             fetch('/bedseriouson')
                 .then(response => response.text())
                 .then(data => console.log(data))
         }
-        function bedrelaxon(){
+        function bedrelaxon() {
             fetch('/bedrelaxon')
                 .then(response => response.text())
                 .then(data => console.log(data))
         }
-        function bedbulboff(){
+        function bedbulboff() {
             fetch('/bedbulboff')
                 .then(response => response.text())
                 .then(data => console.log(data))
         }
-            // Fan
-        function bedFanSlow(){
+        // Fan
+        function bedFanSlow() {
             fetch('/bedFanSlow')
                 .then(response => response.text())
                 .then(data => console.log(data))
-        } 
-        function bedFanMedium(){
+        }
+        function bedFanMedium() {
             fetch('/bedFanMedium')
                 .then(response => response.text())
                 .then(data => console.log(data))
-        } 
-        function bedFanFast(){
+        }
+        function bedFanFast() {
             fetch('/bedFanFast')
                 .then(response => response.text())
                 .then(data => console.log(data))
-        } 
-        function bedFanOff(){
+        }
+        function bedFanOff() {
             fetch('/bedFanOff')
                 .then(response => response.text())
                 .then(data => console.log(data))
         } 
     </script>
 </body>
+
 </html>
 )rawliteral";
 
